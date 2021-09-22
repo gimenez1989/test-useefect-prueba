d1473b5a4bb8f15bc27079ecd063497158335e07
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-09-22 17:01:23 -0300

    proof

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+
+  useEffect(() => {
+    console.log("count")
+    // setCount(count + 1) el contador n para de contar 
+  }, [])
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>Contando: {count}</p>
+          <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
